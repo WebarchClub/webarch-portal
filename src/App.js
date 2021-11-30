43938212c4ad4c0365d6acbbacf1dc453e946abc
Mercer1410 <hs7582@srmist.edu.in>
2021-11-30 22:07:50 +0530

    Base Layout set

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import "./App.css";
 import Main from "./Main";
+import Navbar from "./containers/Navbar/Navbar";
 
 function App() {
     return (
         <Router>
             <Container className="App">
-                {/* NAVBAR */}
+                <Navbar />
                 <Main />
             </Container>
         </Router>
