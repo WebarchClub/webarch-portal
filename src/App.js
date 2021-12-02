cd71ac5c684af50f292de692a3eb32fa5bc1a2ad
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2021-12-02 19:49:54 +0530

    Create store and Alert reducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import { Provider } from "react-redux";
 import "./App.css";
+import store from "./store/index"
 import Main from "./Main";
 import Navbar from "./containers/Navbar/Navbar";
 
@@ -8,8 +10,10 @@ function App() {
     return (
         <Router>
             <Container className="App">
-                <Navbar />
-                <Main />
+                <Provider store={store}>
+                    <Navbar />
+                    <Main />
+                </Provider>
             </Container>
         </Router>
     );
