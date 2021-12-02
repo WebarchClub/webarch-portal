ef31c5b9126f66f93cdbd03ec1ad540e7f3df76a
Mercer1410 <hs7582@srmist.edu.in>
2021-12-03 00:50:26 +0530

    MessageBox Added

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,13 @@
-import { Container } from "react-bootstrap";
 import "./Main.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 import MessageBox from "./containers/MessageBox/index";
 
 function Main() {
     return (
-        <Container className="Main">
+        <div className="Main">
             <h1>Main</h1>
             <MessageBox />
-        </Container>
+        </div>
     );
 }
 
