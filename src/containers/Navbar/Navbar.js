edc039bc8b6d1d2b58714442be800ce93ad785cd
sreyom31 <sreyom31@gmail.com>
2021-12-03 23:45:31 +0530

    :lipstick: navbar update
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Logo from "../../assets/logo.png";
 import "./Navbar.css";
 
-function Navbar() {
-    return <div className="Navbar"></div>;
+function Navigation(props) {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home" className="text-center fs-4">
+                    <img
+                        alt=""
+                        src={Logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block"
+                    />{" "}
+                    Webarch Portal
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto fs-5">
+                        {props.currentUser && (
+                            <Fragment>
+                                <Nav.Link href="#deets" className="mx-2">
+                                    profile <i class="fas fa-user-circle"></i>
+                                </Nav.Link>
+                                <Nav.Link href="#deets" className="mx-2">
+                                    Logout{" "}
+                                    <i className="fas fa-sign-out-alt"></i>
+                                </Nav.Link>
+                            </Fragment>
+                        )}
+                        {!props.currentUser && (
+                            <Fragment>
+                                {" "}
+                                <Nav.Link href="#deets" className="mx-2">
+                                    Login
+                                </Nav.Link>
+                                <Nav.Link
+                                    eventKey={2}
+                                    href="#memes"
+                                    className="mx-2"
+                                >
+                                    Signup
+                                </Nav.Link>
+                            </Fragment>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
-export default Navbar;
+export default Navigation;
