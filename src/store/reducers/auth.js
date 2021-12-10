3e3c096a208f812ef0ab8b5f80c9f97e03e5e4dc
Soumik Chaudhuri <soumik.chaudhuri2000@gmail.com>
2021-12-10 09:10:46 +0530

    Auth reducer and actions added (#11)
    * Auth reducer and actions added
    
    * Format of files done

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,26 @@
+import { SET_CURRENT_USER, LOGOUT } from "../actionTypes";
+
+export const authReducer = (
+    state = { isAuthenticated: false, user: null },
+    action
+) => {
+    switch (action.type) {
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticated: Object.keys(action.payload).length
+                    ? true
+                    : false,
+                user: action.payload
+            };
+        case LOGOUT:
+            return {
+                ...state,
+                isAuthenticated: false,
+                user: null
+            };
+
+        default:
+            return state;
+    }
+};
