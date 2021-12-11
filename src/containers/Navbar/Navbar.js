7898bfae08721d052b545f3aa931bfb0e5935f85
sreyom31 <sreyom31@gmail.com>
2021-12-11 16:33:48 +0530

    misc
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
-import { logout } from "../../store/actions/auth"
+import { logout } from "../../store/actions/auth";
 import { FaRegUserCircle } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
 import Logo from "../../assets/logo.png";
@@ -12,7 +12,7 @@ function Navigation() {
     const dispatch = useDispatch();
     const logoutHandler = () => {
         dispatch(logout());
-    }
+    };
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
@@ -34,7 +34,11 @@ function Navigation() {
                                 <Nav.Link href="/user" className="mx-2">
                                     profile <FaRegUserCircle />
                                 </Nav.Link>
-                                <Nav.Link onClick={logoutHandler} href="/" className="mx-2">
+                                <Nav.Link
+                                    onClick={logoutHandler}
+                                    href="/"
+                                    className="mx-2"
+                                >
                                     Logout <FiLogOut />
                                 </Nav.Link>
                             </Fragment>
