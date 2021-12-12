e6c95b84a9abaa6528d71df539e5f08855638edc
Sreyom Sresaan <sreyom31@gmail.com>
2021-12-12 18:48:54 +0530

    Navbar
    * Develop (#7)
    
    * Create store and Alert reducer
    
    * redux-store
    
    * Added messagebox (#6)
    
    * Develop (#4)
    
    * Create store and Alert reducer
    
    * redux-store
    
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    
    * Added MessageBox
    
    * Added styling to modal
    
    * Added styling to modal
    
    Co-authored-by: Hrithik Saxena <59438115+Mercer1410@users.noreply.github.com>
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    
    * MessageBox Added
    
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    Co-authored-by: Mohammad Muazam <60972077+mmuazam98@users.noreply.github.com>
    
    * Develop (#10)
    
    * Create store and Alert reducer
    
    * redux-store
    
    * Added messagebox (#6)
    
    * Develop (#4)
    
    * Create store and Alert reducer
    
    * redux-store
    
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    
    * Added MessageBox
    
    * Added styling to modal
    
    * Added styling to modal
    
    Co-authored-by: Hrithik Saxena <59438115+Mercer1410@users.noreply.github.com>
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    
    * MessageBox Added
    
    * Added apiCall and setTokenHeader (#9)
    
    * Added MessageBox
    
    * Added styling to modal
    
    * Added styling to modal
    
    * Develop (#7)
    
    * Create store and Alert reducer
    
    * redux-store
    
    * Added messagebox (#6)
    
    * Develop (#4)
    
    * Create store and Alert reducer
    
    * redux-store
    
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    
    * Added MessageBox
    
    * Added styling to modal
    
    * Added styling to modal
    
    Co-authored-by: Hrithik Saxena <59438115+Mercer1410@users.noreply.github.com>
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    
    * MessageBox Added
    
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    Co-authored-by: Mohammad Muazam <60972077+mmuazam98@users.noreply.github.com>
    
    * Added apiCall and setTokenHeader
    
    * Update Main.js
    
    Co-authored-by: Hrithik Saxena <59438115+Mercer1410@users.noreply.github.com>
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    Co-authored-by: Mohammad Muazam <60972077+mmuazam98@users.noreply.github.com>
    
    * React-Router-Dom Updated
    
    * navbar updated and all issues resolved
    
    Signed-off-by: sreyom31 <sreyom31@gmail.com>
    
    * link from react-router-dom used
    
    Signed-off-by: sreyom31 <sreyom31@gmail.com>
    
    * misc route error
    
    Signed-off-by: sreyom31 <sreyom31@gmail.com>
    
    * misc
    
    Co-authored-by: Hrithik Saxena <59438115+Mercer1410@users.noreply.github.com>
    Co-authored-by: pinkman7009 <soumik.chaudhuri2000@gmail.com>
    Co-authored-by: Mohammad Muazam <60972077+mmuazam98@users.noreply.github.com>
    Co-authored-by: Mercer1410 <hs7582@srmist.edu.in>

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { logout } from "../../store/actions/auth";
 import { FaRegUserCircle } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
@@ -16,45 +17,78 @@ function Navigation() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/" className="text-center fs-4">
-                    <img
-                        alt=""
-                        src={Logo}
-                        width="30"
-                        height="30"
-                        className="d-inline-block"
-                    />{" "}
-                    Webarch Portal
+                <Navbar.Brand className="text-center fs-4">
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                        <div className="logo d-flex">
+                            <img
+                                alt=""
+                                src={Logo}
+                                width="30"
+                                height="30"
+                                className="d-inline-block"
+                            />
+                            <p className="text-light mb-0 ms-2 mt-1">
+                                Webarch Portal
+                            </p>
+                        </div>
+                    </Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto fs-5">
                         {isAuth && (
                             <Fragment>
-                                <Nav.Link href="/user" className="mx-2">
-                                    profile <FaRegUserCircle />
+                                <Nav.Link className="mx-2">
+                                    <Link
+                                        to="/user"
+                                        style={{
+                                            textDecoration: "none",
+                                            color: "#fff"
+                                        }}
+                                    >
+                                        Profile <FaRegUserCircle />
+                                    </Link>
                                 </Nav.Link>
+
                                 <Nav.Link
                                     onClick={logoutHandler}
-                                    href="/"
                                     className="mx-2"
                                 >
-                                    Logout <FiLogOut />
+                                    <Link
+                                        to="/"
+                                        style={{
+                                            textDecoration: "none",
+                                            color: "#fff"
+                                        }}
+                                    >
+                                        Logout <FiLogOut />
+                                    </Link>
                                 </Nav.Link>
                             </Fragment>
                         )}
                         {!isAuth && (
                             <Fragment>
-                                {" "}
-                                <Nav.Link href="/login" className="mx-2">
-                                    Login
+                                <Nav.Link className="mx-2">
+                                    <Link
+                                        to="/login"
+                                        style={{
+                                            textDecoration: "none",
+                                            color: "#fff"
+                                        }}
+                                    >
+                                        Login
+                                    </Link>
                                 </Nav.Link>
-                                <Nav.Link
-                                    eventKey={2}
-                                    href="/signup"
-                                    className="mx-2"
-                                >
-                                    Signup
+                                <Nav.Link eventKey={2} className="mx-2">
+                                    <Link
+                                        to="/signup"
+                                        style={{
+                                            textDecoration: "none",
+                                            color: "#fff"
+                                        }}
+                                    >
+                                        Signup
+                                    </Link>
                                 </Nav.Link>
                             </Fragment>
                         )}
